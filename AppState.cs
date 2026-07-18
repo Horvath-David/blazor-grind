4222a9db496c80905006dc33ed5c12221478ac4b
SledDev <sleddev@asd.dog>
2026-07-18 17:22:20 +0200

    initial commit

---
+++ AppState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using Microsoft.Maui.Storage;
+
+namespace GeneralUI;
+
+public class AppState
+{
+    public int? UserId { get; set; } = null;
+    public string? UserName { get; set; } = null;
+
+    public static AppState Load() =>
+        JsonSerializer.Deserialize<AppState>(Preferences.Get("appState", "{}")) ?? new();
+
+    public void Save()
+    {
+        Preferences.Set("appState", JsonSerializer.Serialize(this));
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GeneralUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
